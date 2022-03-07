04acfb6b1249e8ed323389629586111d60a91c61
Larion Turlo <larion.turlo@gmail.com>
2022-03-07 13:55:38 +0400

    Algo: Heap data structure

---
+++ data_structure/heap/heap.go
@@ -0,0 +1,117 @@
+package heap
+
+const (
+	minHeap HeapType = -1
+	maxHeap HeapType = 1
+)
+
+type HeapType int
+
+type Heap struct {
+	heap     []int
+	heapType HeapType
+}
+
+func NewHeap(t HeapType) *Heap {
+	return &Heap{
+		heap:     []int{},
+		heapType: t,
+	}
+}
+
+func parent(i int) int {
+	return i / 2
+}
+func left(i int) int {
+	return i * 2
+}
+func right(i int) int {
+	return i*2 + 1
+}
+func comparator(child, parent int, heapType HeapType) HeapType {
+	if child < parent {
+		return minHeap
+	}
+	if child > parent {
+		return maxHeap
+	}
+	return heapType
+}
+
+func (h *Heap) bubbleUp(i int) {
+	if i >= len(h.heap) {
+		return
+	}
+	p := parent(i)
+
+	comp := comparator(h.heap[p], h.heap[i], h.heapType)
+
+	if h.heapType == comp {
+		return
+	}
+
+	h.heap[i], h.heap[p] = h.heap[p], h.heap[i]
+
+	h.bubbleUp(p)
+}
+
+func (h *Heap) bubbleDown(i int) {
+	if i >= len(h.heap) {
+		return
+	}
+	l := left(i)
+	r := right(i)
+
+	child := l
+	if child < len(h.heap) && r < len(h.heap) {
+		compch := comparator(h.heap[child], h.heap[r], h.heapType)
+
+		if h.heapType != compch {
+			child = r
+		}
+	} else if child >= len(h.heap) {
+		if r >= len(h.heap) {
+			return
+		}
+		child = r
+	}
+
+	comp := comparator(h.heap[i], h.heap[child], h.heapType)
+
+	if h.heapType == comp {
+		return
+	}
+
+	h.heap[i], h.heap[child] = h.heap[child], h.heap[i]
+
+	h.bubbleDown(child)
+}
+
+// Function return heap as slice
+func (h *Heap) AsSlice() []int {
+	return h.heap
+}
+
+// Function for heapify slice
+func (h *Heap) Heapify(arr []int) {
+	h.heap = arr
+	for i := len(arr) - 1; i >= 0; i-- {
+		h.bubbleDown(i)
+	}
+}
+
+// Function insert a number to the heap
+func (h *Heap) Insert(n int) {
+	h.heap = append(h.heap, n)
+	h.bubbleUp(len(h.heap) - 1)
+}
+
+// Function delete a number to the heap
+func (h *Heap) Delete(i int) {
+	if i < 0 || i > len(h.heap)-1 {
+		return
+	}
+	h.heap[i], h.heap[len(h.heap)-1] = h.heap[len(h.heap)-1], h.heap[i]
+	h.heap = h.heap[:len(h.heap)-1]
+	h.bubbleDown(i)
+}
---
+++ data_structure/heap/heap_test.go
@@ -0,0 +1,181 @@
+package heap
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestHeap_Heapify(t *testing.T) {
+	type fields struct {
+		heapType HeapType
+	}
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []int
+	}{
+		{
+			"MinHeap",
+			fields{
+				minHeap,
+			},
+			args{
+				[]int{5, 4, 4, 3, 3, 2, 2, 1},
+			},
+			[]int{1, 2, 3, 2, 5, 4, 4, 3},
+		},
+		{
+			"MaxHeap",
+			fields{
+				maxHeap,
+			},
+			args{
+				[]int{1, 2, 3, 2, 5, 4, 4, 3},
+			},
+			[]int{5, 4, 3, 4, 1, 2, 2, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewHeap(tt.fields.heapType)
+			h.Heapify(tt.args.arr)
+			if got := h.AsSlice(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("heapify() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHeap_Insert(t *testing.T) {
+	type fields struct {
+		heap     []int
+		heapType HeapType
+	}
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []int
+	}{
+		{
+			"minHeap",
+			fields{
+				[]int{},
+				minHeap,
+			},
+			args{5},
+			[]int{5},
+		},
+		{
+			"minHeap",
+			fields{
+				[]int{4, 5},
+				minHeap,
+			},
+			args{3},
+			[]int{3, 4, 5},
+		},
+		{
+			"maxHeap",
+			fields{
+				[]int{4, 3},
+				maxHeap,
+			},
+			args{5},
+			[]int{5, 4, 3},
+		},
+		{
+			"maxHeap",
+			fields{
+				[]int{5, 4, 3, 1, 2, 2, 3},
+				maxHeap,
+			},
+			args{4},
+			[]int{5, 4, 3, 4, 2, 2, 3, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := &Heap{
+				heap:     tt.fields.heap,
+				heapType: tt.fields.heapType,
+			}
+			h.Insert(tt.args.n)
+			if got := h.AsSlice(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("heapify() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHeap_Delete(t *testing.T) {
+	type fields struct {
+		heap     []int
+		heapType HeapType
+	}
+	type args struct {
+		i int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []int
+	}{
+		{
+			"minHeap",
+			fields{
+				[]int{},
+				minHeap,
+			},
+			args{0},
+			[]int{},
+		},
+		{
+			"minHeap",
+			fields{
+				[]int{3, 4, 5},
+				minHeap,
+			},
+			args{0},
+			[]int{4, 5},
+		},
+		{
+			"maxHeap",
+			fields{
+				[]int{5, 4, 3},
+				maxHeap,
+			},
+			args{0},
+			[]int{4, 3},
+		},
+		{
+			"maxHeap",
+			fields{
+				[]int{5, 4, 3, 4, 2, 2, 3, 1},
+				maxHeap,
+			},
+			args{3},
+			[]int{5, 4, 3, 3, 2, 2, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := &Heap{
+				heap:     tt.fields.heap,
+				heapType: tt.fields.heapType,
+			}
+			h.Delete(tt.args.i)
+			if got := h.AsSlice(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("heapify() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
