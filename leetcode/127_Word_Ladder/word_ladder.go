0242437e779b13f0c18805a13880d883db7246ea
Larion Turlo <larion.turlo@gmail.com>
2022-04-14 18:56:11 +0400

    LeetCode: correct solving for 127 word ladder

--- leetcode/127_Word_Ladder/word_ladder.go
+++ leetcode/127_Word_Ladder/word_ladder.go
@@ -12,48 +12,33 @@ func stringsDiff(a, b string) int {
 	return diffLetterCount
 }
 
-type layer map[string]string
-
 // Problem https://leetcode.com/problems/word-ladder/
 func ladderLength(beginWord string, endWord string, wordList []string) int {
-	layerList := make([]layer, len(wordList), len(wordList))
-	minPathLen := 0
-	temp := make(map[string]int)
-	for i := range wordList {
-		if wordList[i] == endWord {
-			wordList[i], wordList[len(wordList)-1] = wordList[len(wordList)-1], wordList[i]
-		} else if wordList[i] == beginWord {
-			wordList[i], wordList[0] = wordList[0], wordList[i]
+	q := make([]string, 1)
+	d := make(map[string]int)
+	m := make(map[string]struct{})
+
+	q[0] = beginWord
+	d[beginWord] = 1
+	m[beginWord] = struct{}{}
+
+	for len(q) > 0 {
+		w := q[0]
+		q = q[1:]
+
+		for i := 0; i < len(wordList); i++ {
+			diff := stringsDiff(w, wordList[i])
+
+			_, marked := m[wordList[i]]
+			if diff < 2 && !marked {
+				d[wordList[i]] = d[w] + 1
+				m[wordList[i]] = struct{}{}
+				q = append(q, wordList[i])
+			}
 		}
 	}
 
-	for i := 0; i < len(wordList); i++ {
-		layer := stringsDiff(beginWord, wordList[i])
-		beforLayerIndex := layer - 1
-		beforWord := beginWord
-
-		if beforLayerIndex > 1 {
-			for word := range layerList[beforLayerIndex] {
-				if stringsDiff(word, wordList[i]) == 1 {
-					beforWord = word
-					break
-				}
-			}
-			if beforWord == beginWord {
-				temp[wordList[i]] = layer
-			}
-		}
-
-		if endWord == wordList[i] {
-			minPathLen = layer
-		}
-
-		if len(layerList[layer]) == 0 {
-			layerList[layer] = make(map[string]string)
-		}
-
-		layerList[layer][wordList[i]] = beforWord
-	}
+	minPathLen := d[endWord]
 
 	return minPathLen
 }
